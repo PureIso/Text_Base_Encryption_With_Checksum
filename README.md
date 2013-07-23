062401ab4587514d2ba09d6eb69b66c79293676b
PureIso <olaegby@live.com>
2013-07-23 21:09:15 +0100

    Initial Commit
    Initial Commit fully working

---
+++ README.md
@@ -0,0 +1,12 @@
+#Text Base Encryption With Checksum#
+Experimental application that encodes text using encryption and checksum
+
+## How to use:###
+~~~
+Compile and run application
+~~~
+
+TODO
+~~~
+Add a build
+~~~
---
+++ TextBaseEncryption/TextBaseEncryption/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace TextBaseEncryption
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataTextBox = new System.Windows.Forms.TextBox();
+            this.decryptButton = new System.Windows.Forms.Button();
+            this.encryptButton = new System.Windows.Forms.Button();
+            this.checksumComboBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // dataTextBox
+            // 
+            this.dataTextBox.Location = new System.Drawing.Point(12, 12);
+            this.dataTextBox.Multiline = true;
+            this.dataTextBox.Name = "dataTextBox";
+            this.dataTextBox.Size = new System.Drawing.Size(592, 115);
+            this.dataTextBox.TabIndex = 0;
+            this.dataTextBox.Text = "My name is Olawale Egbeyemi";
+            // 
+            // decryptButton
+            // 
+            this.decryptButton.Location = new System.Drawing.Point(529, 161);
+            this.decryptButton.Name = "decryptButton";
+            this.decryptButton.Size = new System.Drawing.Size(75, 23);
+            this.decryptButton.TabIndex = 1;
+            this.decryptButton.Text = "Decrypt";
+            this.decryptButton.UseVisualStyleBackColor = true;
+            this.decryptButton.Click += new System.EventHandler(this.decryptButton_Click);
+            // 
+            // encryptButton
+            // 
+            this.encryptButton.Location = new System.Drawing.Point(448, 161);
+            this.encryptButton.Name = "encryptButton";
+            this.encryptButton.Size = new System.Drawing.Size(75, 23);
+            this.encryptButton.TabIndex = 2;
+            this.encryptButton.Text = "Encrypt";
+            this.encryptButton.UseVisualStyleBackColor = true;
+            this.encryptButton.Click += new System.EventHandler(this.encryptButton_Click);
+            // 
+            // checksumComboBox
+            // 
+            this.checksumComboBox.FormattingEnabled = true;
+            this.checksumComboBox.Items.AddRange(new object[] {
+            "Adler 32",
+            "CRC 32",
+            "CRC 32 Jam"});
+            this.checksumComboBox.Location = new System.Drawing.Point(111, 168);
+            this.checksumComboBox.Name = "checksumComboBox";
+            this.checksumComboBox.Size = new System.Drawing.Size(121, 21);
+            this.checksumComboBox.TabIndex = 3;
+            this.checksumComboBox.Text = "Select Checksum";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 171);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Select Checksum:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(49, 137);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Password:";
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(112, 134);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(120, 20);
+            this.passwordTextBox.TabIndex = 6;
+            this.passwordTextBox.Text = "olawale";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(616, 196);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checksumComboBox);
+            this.Controls.Add(this.encryptButton);
+            this.Controls.Add(this.decryptButton);
+            this.Controls.Add(this.dataTextBox);
+            this.Name = "Form1";
+            this.Text = "Text Base Encryption";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox dataTextBox;
+        private System.Windows.Forms.Button decryptButton;
+        private System.Windows.Forms.Button encryptButton;
+        private System.Windows.Forms.ComboBox checksumComboBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox passwordTextBox;
+    }
+}
+
